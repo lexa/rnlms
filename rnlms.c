1a5b607c024be4ac02596717dc0c2387f7cee67d
lexa <lexa@desktop.cfotr.com>
2011-08-29 22:39:24 +0400

    исправлена ошибка неверного вычисления размера памяти для фильтра

--- rnlms.c
+++ rnlms.c
@@ -35,7 +35,7 @@ NUM SIGN(NUM a)
 size_t rlms_sizeOfRequiredMemory(size_t filter_len)
 {
 	return sizeof(SimpleIIRFilter) +	\
-		(sizeof(NUM)) +	\
+		(sizeof(NUM)*filter_len) +	\
 		(CB_size(filter_len)) ;
 }
 
