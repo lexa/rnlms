862673c4fd92fb438eba165686b4f20eac545ac5
lexa <lexa@cfotr.com>
2011-08-29 22:10:19 -0700

    add linker command file

---
+++ rnlms.cmd
@@ -0,0 +1,30 @@
+/*
+ *  Copyright 2001 by Texas Instruments Incorporated.
+ *  All rights reserved. Property of Texas Instruments Incorporated.
+ *  Restricted rights to use, duplicate or disclose this code are
+ *  granted through contract.
+ */
+/*
+ *  ======== hello.cmd ========
+ *
+ */
+
+
+MEMORY 
+{
+   IRAM       : origin = 0x0,         len = 0x10000
+}
+
+SECTIONS
+{
+        .vectors > IRAM
+        .text    > IRAM
+
+        .bss     > IRAM
+        .cinit   > IRAM
+        .const   > IRAM
+        .far     > IRAM
+        .stack   > IRAM
+        .cio     > IRAM
+        .sysmem  > IRAM
+}
